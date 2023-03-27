121791755271a687a2198d608af55a48ceb848b9
TARAK <tarakaramtirumala@gmail.com>
2023-03-27 16:03:10 +0530

    changes

--- main.tf
+++ main.tf
@@ -8,12 +8,21 @@ resource "aws_rds_cluster" "main" {
   backup_retention_period = var.backup_retention_period
   preferred_backup_window = var.preferred_backup_window
   db_subnet_group_name = aws_db_subnet_group.main.name
+
  tags = merge(
       var.tags,
         { Name = "${var.env}-rds" }
     )
 }
 
+resource "aws_rds_cluster_instance" "main" {
+  count              = var.no_of_instances
+  identifier         = "${var.env}-rds-${count.index}"
+  cluster_identifier = aws_rds_cluster.main.id
+  instance_class     = var.instance_class
+  engine                  = var.engine
+  engine_version          = var.engine_version
+}
 
 
 resource "aws_db_subnet_group" "main" {
--- vars.tf
+++ vars.tf
@@ -9,3 +9,5 @@ variable "backup_retention_period" {}
 variable "preferred_backup_window" {}
 variable "engine" {}
 variable "engine_version" {}
+variable "no_of_instances" {}
+variable "instance_class" {}
